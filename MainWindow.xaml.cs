c6585d0c9d576719e2bea05cf8942019bad71851
johanwintgens <peoplecallmeshady@hotmail.com>
2015-03-19 19:58:40 +0100

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace SQLlite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private SQLiteConnection mDbconnection;
+
+        
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            
+            mDbconnection = new SQLiteConnection("Data Source=school_sqlite;Version=3;");
+
+            mDbconnection.Open();
+
+            string sql = "create table highscores (name varchar(20), score int)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, mDbconnection);
+            command.ExecuteNonQuery();
+
+            sql = "select * from highscores order by score desc";
+            command = new SQLiteCommand(sql, mDbconnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            }
+
+            mDbconnection.Close();
+        }
+
+     
+    }
+}
